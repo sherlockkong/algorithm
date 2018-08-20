f76475026da1a531940860b8228d9055d6b2b801
sherlock kong <sherlock.kong@grapecity.com>
2018-08-20 22:49:53 +0800

    add license and readme.

--- src/other/generate-cards-origin-order.js
+++ src/other/generate-cards-origin-order.js
@@ -5,7 +5,7 @@
  * 	
  * 	Description: 
  *  一副从1到n的牌，每次从牌堆顶取一张放桌子上，再取一张放牌堆底，直到牌堆没牌，
- * 	最后桌子上的牌是从1到n有序，设计程序，输入n，输出牌堆的顺序数组
+ * 	最后桌子上的牌是从1到n有序(最底下是1)，设计程序，输入n，输出牌堆的顺序数组
  */
 
 export default function generateCardsOriginOrder(n) {
